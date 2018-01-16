2adb6eb642d8d74335299873e89ed9a8231cc5d7
WoodNeck <saiyu915@naver.com>
2018-01-16 15:50:31 +0900

    feat: Adding listener

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
+import time
 import logging
 import discord
+import asyncio
+from apscheduler.schedulers.background import BackgroundScheduler
 from utils.botconfig import BotConfig
 from discord.ext import commands
 from selenium import webdriver
@@ -17,12 +20,16 @@ class Bot(commands.Bot):
         self.prefix = prefix
         options = Options()  
         options.add_argument("--headless") 
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome(chrome_options=options)
         self.naverId = None
         self.naverPwd = None
         self.target = None
+        self.textArea = None
+        self.sendBtn = None
+        self.lastMsg = None
+        self.channel = None
     
-    def loginToChattingRoom():
+    def loginToChattingRoom(self):
         self.driver.get(self.target)
         user = self.driver.find_element_by_id("id")
         password = self.driver.find_element_by_id("pw")
@@ -31,10 +38,29 @@ class Bot(commands.Bot):
         loginBtn = self.driver.find_element_by_class_name("btn_global")
         loginBtn.click()
         
-        print(self.driver.current_url)
-    
-    def sendToChattingRoom():
+        self.textArea = self.driver.find_element_by_id("msgInputArea")
+        self.sendBtn = self.driver.find_elements_by_tag_name("button")[4]
 
+        time.sleep(3)
+        messages = self.driver.find_elements_by_class_name("msg")
+        self.lastMsg = int(messages[-1].get_attribute("msgsn"))
+
+    def listenToChattingRoom(self):
+        try:
+            while (True):
+                msg = self.driver.find_element_by_xpath("//div[@msgsn='{}']".format(self.lastMsg + 1))
+                self.lastMsg = int(msg.get_attribute("msgsn"))
+                if not "my" in msg.get_attribute("class"):
+                    author = msg.find_element_by_class_name("name").text
+                    content = msg.find_element_by_class_name("_chat_msg").text
+                    asyncio.run_coroutine_threadsafe(self.send_message(self.channel, "{}/{}".format(author, content)),
+                                                    self.loop)
+        except:
+            return
+    
+    def sendToChattingRoom(self, msg):
+        self.textArea.send_keys(msg)
+        self.sendBtn.click()
     
 def initialize(bot_class=Bot):
     bot = bot_class(description=des)
@@ -42,9 +68,17 @@ def initialize(bot_class=Bot):
     @bot.event
     async def on_ready():
         bot.loginToChattingRoom()
+        print(bot.servers)
+        server = None
+        for item in bot.servers:
+            server = item
+        bot.channel = server.default_channel
         print("{}".format(discord.version_info))
         print("{} 준비 다됬어용".format(bot.user))
         await bot.change_presence(game=discord.Game(name="{}".format(prefix)))
+        listener = BackgroundScheduler()
+        listener.start()
+        listener.add_job(bot.listenToChattingRoom, 'interval', seconds = 1)
 
     @bot.event
     async def on_message(message):
@@ -53,9 +87,12 @@ def initialize(bot_class=Bot):
         if not message.content.startswith(bot.prefix):
             return
         
-        content = message[1:]
+        author = message.author.nick
+        if not author:
+            author = message.author.name
+        content = message.content[1:]
         if content:
-            bot.sendToChattingRoom(content)
+            bot.sendToChattingRoom("{}/{}".format(author, content))
 
     @bot.event
     async def on_command_error(error, ctx):
@@ -80,8 +117,6 @@ def initialize(bot_class=Bot):
                                             "{:.2f}초 뒤에 다시 해주세용"
                                             "".format(error.retry_after))
     return bot
-
-
 
 if __name__ == '__main__':
     bot = initialize()
